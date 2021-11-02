42922ba6e87033e8ca4fd70c935288a4fc907c2a
Mykal Machon <mykal@tinybox.dev>
2021-11-01 22:48:31 -0700

    fix: adding password specific hash function
    see #1 for more info

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,10 @@
-import { hashFunction } from "./encryption"
+import crypto from 'crypto';
+
+const passHashFunction = (key: string) => {
+  const hasher = crypto.createHash('sha512');
+  hasher.update(key);
+  return hasher.digest('hex');
+}
 
 // Types needed for module
 type FailedAuth = {
@@ -16,14 +22,14 @@ type SuccessfulAuth = {
 // Module Functions
 export const createPassword = (newPass: string) => {
   // Hash the password
-  const hashPass = hashFunction(newPass);
+  const hashPass = passHashFunction(newPass);
   // store the password locally
   // TODO: store the password using lowdb
 }
 
 export const authenticate = (password: string): SuccessfulAuth | FailedAuth | NoUser  => {
   // Hash the password
-  const hashPass = hashFunction(password);
+  const hashPass = passHashFunction(password);
   // Fetch hashed password from storage (if NA, create password)
   try{
     // TODO: fetch password using lowdb
