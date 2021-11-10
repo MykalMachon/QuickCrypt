0fb37347127b82af95455bd9581bc18810b7e6ac
Mykal Machon <mykal@tinybox.dev>
2021-11-09 21:30:28 -0800

    refactor(commands): laying out new structure
    setup a layout for decoupling inputs from logic

---
+++ src/inputs.ts
@@ -0,0 +1,17 @@
+// this is where we do all the fancy inquirer stuff
+
+const createPasswordFlow = () => {
+  // TODO: move inquirer / user flow actions into this method
+};
+
+const listAllPasswordsFlow = () => {
+  // TODO: move inquirer / user flow actions into this method
+};
+
+const getPasswordByKeyFlow = () => {
+  // TODO: move inquirer / user flow actions into this method
+};
+
+const isAuthenticatedFlow = () => {
+  // TODO: move inquirer / user flow actions into this method
+};
